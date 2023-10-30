e7c5194df54e42382efab44d04237a0f474d7a06
Tsubasa Watanabe <t.watanabe423@gmail.com>
2023-10-30 06:11:33 +0000

    upd

--- app/src/components/atoms/SimpleBarcode.tsx
+++ app/src/components/atoms/SimpleBarcode.tsx
@@ -9,19 +9,20 @@ type SimpleBarcodeProps = {
 
 export const SimpleBarcode: React.FC<SimpleBarcodeProps> = ({ value, format }) => {
   const barcodeRef = useRef(null);
+  const widthValue = format === 'CODE128' ? 1 : 1.5
 
   useEffect(() => {
     if (barcodeRef.current) {
       JsBarcode(barcodeRef.current, value, {
         format: format, // 例: CODE128の形式。他の形式も選択可能。 EAN13 EAN8
-        width: 0.5,
+        width: widthValue,
         height: 30,
         displayValue: true, // バーコードの下にテキストとしての価値を表示するかどうか
         font: 'noto sans',
         fontSize: 14
       });
     }
-  }, [value, format]);
+  }, [value, format, widthValue]);
 
   return (
     <Box m='6'>
